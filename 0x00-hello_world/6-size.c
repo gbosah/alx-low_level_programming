ec08c7789d017cb3d61792a622f373ad8af8c17a
gbosah <gbosah@gmail.com>
2022-09-08 20:45:38 +0000

    Alx low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,9 @@ int main(void)
 	long long int  d;
 	float e;
 	printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a int: %1u byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(e));
-
-	return (0);
+i	return (0);
 }
