3df84bedaed461bf9a3ccfc7fa10681c2c437246
gbosah <gbosah@gmail.com>
2022-09-09 20:27:24 +0000

    Alx low level programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
