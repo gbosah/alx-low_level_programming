8e867b1d4a2807a20ab19c33f19a6295aedbf3e9
gbosah <gbosah@gmail.com>
2022-09-10 19:52:07 +0000

    Alx low level programming

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num, num2;
+
+	for (num = 0; num < 98; num++)
+	{
+		for (num2 = num + 1; num2 < 99; num2++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+		if (num == 98 && num2 == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
