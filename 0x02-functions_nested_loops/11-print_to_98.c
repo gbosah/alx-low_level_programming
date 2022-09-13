87c09ac11860f941c5f1bcd3fb8f39d9b99f9b63
gbosah <gbosah@gmail.com>
2022-09-13 22:48:30 +0000

    Alx low level programming

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,11 +14,14 @@ void print_to_98(int n)
 	{
 		for (n = n; n < 98; n++)
 			printf("%d, ", n);
-			printf("%d\n ", n);
+			printf("%d\n ", 98);
 	}
 	else
+	{
+
 		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-			printf("%d\n ", n);
+			printf("%d\n ", 98);
+	}
 
 }
