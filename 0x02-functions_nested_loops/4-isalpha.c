3f68552b610f46f0908f34813e76d1b49cd9d8d9
gbosah <gbosah@gmail.com>
2022-09-13 15:16:06 +0000

    Alx function and nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- * _isalaplha - a function that checks for alpha character
+ * _isalpha - a function that checks for alpha character
  * @c: single letter input
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
