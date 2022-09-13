d681b07c69706f09d186ab596147e509d2f896f3
gbosah <gbosah@gmail.com>
2022-09-13 20:48:28 +0000

    Alx low level programming

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - a function to print 00:00 to 23:59
+ * Return: 1 if int c is lowercase, 10 if otherwise
+ */
+
+void jack_bauer(void)
+{
+
+	int i, j;
+
+	for (i = 10; i < 24; i++)
+	{
+		for (j = 10; j < 60; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
