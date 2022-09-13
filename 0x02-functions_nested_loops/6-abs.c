dca44f5c44710d49923889ff6325313d0bd594fd
gbosah <gbosah@gmail.com>
2022-09-13 16:04:16 +0000

    Alx function and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - a function that checks for lowercase character
+ * @i: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+
+int _abs(int i)
+{
+
+	if (abs(i))
+	{
+		return (i);
+	}
+	return (0);
+}
