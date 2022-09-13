d2f741e90e444430f96c6c096a970b0ac9363ce1
gbosah <gbosah@gmail.com>
2022-09-13 22:25:24 +0000

    Alx low level programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - a function that add two number
+ * @num1 : single letter input
+ * @num2 : single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+
+int add(int num1, int num2)
+{
+	int res;
+
+	res = num1 + num2;
+	_putchar(res);
+	return (0);
+
+}
