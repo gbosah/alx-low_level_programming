0973abde876e54afa1b4d07d8b9882aa54a49e5d
gbosah <gbosah@gmail.com>
2022-09-14 20:43:29 +0000

    Alx low level programming

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,19 +7,19 @@
 * positive_or_negative - tests function that prints if integer is
 * positive_or_negative
 *
-* @n: The input number
+* @i: The input number
 */
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
 	else
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 }
