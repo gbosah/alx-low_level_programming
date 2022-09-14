1fc3a7446069e4612368845caddc3f9242d8f25c
gbosah <gbosah@gmail.com>
2022-09-14 21:26:10 +0000

    Alx low level programming

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
 
 /**
-* positive_or_negative - tests function that prints if integer is
-* positive_or_negative
-*
-* @i: The input number
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
 
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (i < 0)
-		printf("%d is negative\n", i);
-	else if (i > 0)
-		printf("%d is positive\n", i);
-	else
-		printf("%d is zero\n", i);
+	i = 98;
+	positive_or_negative(i);
+	return (0);
 }
