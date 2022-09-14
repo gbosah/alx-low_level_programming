7d100e29d4c544f7efe97fd09b48f48840272460
gbosah <gbosah@gmail.com>
2022-09-14 19:28:02 +0000

    Alx low level programming

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,10 @@ int main(void)
 	int i, res;
 
 	for (i = 0; i < 1024; i++)
-		if (i % 3 == 0 && i % 5 == 0)
+	{
+		for (i = 0; i < 1024; i++)
 			res += i;
 		printf("%d\n", res);
-		return (0);
-
+	}
+	return (0);
 }
