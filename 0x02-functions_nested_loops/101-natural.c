bf33ff0c49a35072b49b17c7260523f8f5054f30
gbosah <gbosah@gmail.com>
2022-09-14 19:21:47 +0000

    Alx low level programming

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that computes and prints all the
+ * multiples of 3 and 5 below 1024
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int i, res;
+
+	for (i = 0; i < 1024; i++)
+
+		if (i % 3 == 0 && i % 5 == 0)
+			res += i;
+		printf("%d\n", res);
+		return (0);
+
+}
