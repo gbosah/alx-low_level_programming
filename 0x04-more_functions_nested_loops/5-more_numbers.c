fcb4574f7a392ae16c6b16f11fa0667ed37961ca
gbosah <gbosah@gmail.com>
2022-09-15 19:35:26 +0000

    Alx low level programming

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing from 0 - 9
+ *
+ *
+ * Return: return 1 or 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j < 15; i++)
+		{
+
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+
+}
