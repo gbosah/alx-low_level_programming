9c7ae22499cee667afe626386fa1321db3e3ac40
gbosah <gbosah@gmail.com>
2022-09-15 20:32:08 +0000

    Alx low level programming

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,12 +14,9 @@ void print_line(int n)
 	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
-			if (n == 0 || n < 0)
-			{
-				_putchar('\n');
-
-			}
 
 	}
+	if (n == 0 || n < 0)
+		_putchar('\n');
 
 }
