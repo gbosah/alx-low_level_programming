a422aa04c4c34bb6b756ebf664c626dbb65d22b4
gbosah <gbosah@gmail.com>
2022-09-15 22:05:40 +0000

    Alx low level programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+		printf("%d", i);
 		printf(" ");
-		printf("\n");
 	}
+	printf("\n");
+	return (0);
 
 }
