be756b223a7d355a72020f7ad1771b114a61b789
gbosah <gbosah@gmail.com>
2022-09-15 21:32:54 +0000

    Alx low level programming

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Executable function goes here
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("FizzBuzz");
+		printf(" ");
+		printf("\n");
+	}
+
+}
