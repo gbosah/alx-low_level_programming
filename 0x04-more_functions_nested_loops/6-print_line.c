838d0862eb7745322d08268d528880fd86620133
gbosah <gbosah@gmail.com>
2022-09-15 20:16:41 +0000

    Alx low level programming

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal.
+ * @n: single input line
+ *
+ * Return: return 1 or 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+			if (n == 0 || n < 0)
+			{
+				_putchar('\n');
+
+			}
+
+	}
+
+}
