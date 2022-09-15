6bc851e02f71ae2011b7fcc4676a091dd4810177
gbosah <gbosah@gmail.com>
2022-09-15 22:20:31 +0000

    Alx low level programming

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle -  a function that prints triangle, follows by a line.
+ * @size: single input line
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < (size - 1); i++)
+		{
+			for (j = 0; j < (size - 1) - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < j; k++)
+			{
+				_putchar('#');
+
+			}
+
+			_putchar('\n');
+
+		}
+	}
+}
