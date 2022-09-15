f67d735a4d30e12db725845a7d667ae5716c1504
gbosah <gbosah@gmail.com>
2022-09-15 18:00:17 +0000

    Alx low level programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplication of two integers
+ * @a: single letter input
+ * @b: single letter input
+ *
+ * Return: return 1 or 0
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
