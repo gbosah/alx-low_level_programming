0430185a8616e57b5a1be28ab51cf385766cfe3b
gbosah <gbosah@gmail.com>
2022-09-15 18:14:19 +0000

    Alx low level programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing from 0 - 9
+ *
+ *
+ * Return: return 1 or 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
