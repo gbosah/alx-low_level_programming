f27948bf9ae4636dea4a98a797d3451c32badff8
gbosah <gbosah@gmail.com>
2022-09-17 19:27:32 +0000

    Alx low level programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,84 @@
 #include "main.h"
-/**
- * print_number - prints a number
- * @n: Input number
- */
 
+
+int _pow(int a, int b);
+
+/**
+ * print_number - print the given number.
+ * @n: number to print.
+ */
 void print_number(int n)
 {
-	unsigned int len, res, i, temp, expo;
+	int collection = n;
+	unsigned int new_num;
+	int count = 0;
+	int print = 0;
 
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
+	if (n < 0)
 	{
-		res *= -1;
+		new_num = -n;
 		_putchar('-');
 	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
+	else
 	{
-		len++;
-		temp /= 10;
+		new_num = n;
 	}
 
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
+	while (collection != 0)
 	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
+		collection /= 10;
+		++count;
 	}
-	_putchar(res % 10 + '0');
+	count -= 1;
+
+	do {
+		if ((new_num / (_pow(10, count)) > 9) && count > 1)
+		{
+			print = new_num / (_pow(10, count));
+			print = print % 10;
+			_putchar('0' + print);
+		}
+		else if (count > 1)
+		{
+			print = (new_num / _pow(10, count));
+		       _putchar('0' + print);
+		}
+		else if (((new_num / 10) > 9) && count == 1)
+		{
+			print = (new_num / 10) % 10;
+			_putchar('0' + print);
+		}
+		else if (count == 1)
+		{
+			print = new_num / 10;
+			_putchar('0' + print);
+		}
+		else
+		{
+			print = new_num % 10;
+			_putchar('0' + print);
+		}
+		count--;
+	} while (count > -1);
 }
+
+/**
+ * _pow - power fuction
+ * @a: value to use.
+ * @b: growth count
+ *
+ * Return: value int.
+ */
+int _pow(int a, int b)
+{
+	int i;
+	int p = a;
+
+	for (i = 1 ; i < b ; i++)
+	{
+		a *= p;
+	}
+	return (a);
+}
+
+
