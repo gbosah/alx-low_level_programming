d756a7aa94fab9df0cc89c8f15d66fd3eee1bb64
gbosah <gbosah@gmail.com>
2022-09-17 19:00:40 +0000

    Alx low level programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,33 +6,13 @@
 
 void print_number(int n)
 {
-	int len, res, i, temp, expo;
-
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
+	if (n >= 0)
 	{
-		res *= -1;
-		_putchar('-');
+		_putchar(n);
 	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
+	else
 	{
-		len++;
-		temp /= 10;
+	_putchar(n);
 	}
-
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+	_putchar('\n');
 }
