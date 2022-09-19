1d0b9a38a3f11476a24ec4937c8d0f0b80427069
gbosah <gbosah@gmail.com>
2022-09-19 11:30:58 +0000

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swap variables
+ *
+ * @a: simple input parameter
+ * @b: simple input parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+
+
+}
+
