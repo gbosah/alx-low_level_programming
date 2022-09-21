8e427c0bf330363ca229c85bd5ff43424907d07e
gbosah <gbosah@gmail.com>
2022-09-21 18:04:06 +0000

    Pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings.
+ *
+ * @dest: simple  input parameter
+ * @src:  simple input parameter
+ *
+ * Return: return char.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
