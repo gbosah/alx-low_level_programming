f21fa38af4ccc6227b5fba27113e7ba1ac294fd2
gbosah <gbosah@gmail.com>
2022-09-26 17:38:41 +0000

    Pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memcpy -  a function that fills memory with a constant byte.
+ *
+ * @dest: pointer to the destination
+ * @src: source of the value
+ * @n: Number of bytes to be filled starting from s to be filled.
+ *
+ * Return: return char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	char *pdest = (char *)dest;
+	char *psrc = (char *)src;
+
+	for (i = 0; i < n; i++)
+		pdest[i] = psrc[i];
+	return (dest);
+}
