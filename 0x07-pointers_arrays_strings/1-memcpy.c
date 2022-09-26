a24581433a2e04e1948f76acb2fa72106d73ca86
gbosah <gbosah@gmail.com>
2022-09-26 17:43:15 +0000

    Pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,8 +14,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
 
-	char *pdest = (char *)dest;
-	char *psrc = (char *)src;
+	char *pdest = dest;
+	char *psrc = src;
 
 	for (i = 0; i < n; i++)
 		pdest[i] = psrc[i];
