41b045c30a2fc4079f122fe3fba94b3861bb6155
gbosah <gbosah@gmail.com>
2022-09-28 19:50:11 +0000

    Recursion in c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - a function that prints a string, followed by a new line.
+ * @s: parameter that accept input
+ * Return: return (0)
+ */
+
+int _strlen_recursion(char *s)
+{
+	_putchar(strlen(s));
+
+	return (0);
+}
