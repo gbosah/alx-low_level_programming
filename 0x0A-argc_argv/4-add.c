b96187b4067574a0aeccc537f7897e26e92ee647
gbosah <gbosah@gmail.com>
2022-09-30 19:06:57 +0000

    argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that run other function.
+ *
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+	{
+		puts("0");
+	}
+
+	if (argc > 'a' || argc < 'z')
+	{
+		puts("Error");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum +=  atoi(argv[i]);
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
