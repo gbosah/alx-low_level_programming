612dd72137b2776efa2751145c4997f3cd545a0a
gbosah <gbosah@gmail.com>
2022-09-30 18:25:50 +0000

    argc and argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a function that run other function.
+ *
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
