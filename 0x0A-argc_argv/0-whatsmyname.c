845597084c8a5c7e8da6841c7e4a2baafad47306
gbosah <gbosah@gmail.com>
2022-09-30 17:52:48 +0000

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - a function that run other function.
+ *
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
