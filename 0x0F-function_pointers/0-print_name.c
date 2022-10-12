27a013c330ca3dab98164d187956b5aa243db4d5
gbosah <gbosah@gmail.com>
2022-10-12 18:49:25 +0000

    Function pointer in c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - A function that prints a name
+ * @name: A name to print
+ * @f: A function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
