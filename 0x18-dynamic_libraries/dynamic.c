e45f898d1c2186420749fc36b726668d731d95e8
gbosah <gbosah@gmail.com>
2022-12-14 18:30:05 +0000

    corrected

---
+++ 0x18-dynamic_libraries/dynamic.c
@@ -0,0 +1,213 @@
+/**
+ * _putchar - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _putchar(__attribute__((unused)) char c)
+{
+
+}
+/**
+ * _islower - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _islower(__attribute__((unused)) int c)
+{
+
+}
+/**
+ * _isalpha - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _isalpha(__attribute__((unused)) int c)
+{
+
+}
+/**
+ * _abs - prints char
+ * @n: char to print
+ * Return: always 1
+ */
+
+int _abs(__attribute__((unused)) int n)
+{
+
+}
+/**
+ * _isupper - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _isupper(__attribute__((unused)) int c)
+{
+
+}
+/**
+ * _isdigit - prints char
+ * @c: char to print
+ * Return: always 1
+ */
+
+int _isdigit(__attribute__((unused)) int c)
+{
+
+}
+/**
+ * _strlen - prints char
+ * @s: char to print
+ * Return: always 1
+ */
+
+int _strlen(__attribute__((unused)) char *s)
+{
+
+}
+/**
+ * _puts - prints char
+ * @s: char to print
+ * Return: always 1
+ */
+void _puts(__attribute__((unused)) char *s)
+{
+
+}
+/**
+ * *_strcpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * Return: always 1
+ */
+
+char *_strcpy(__attribute__((unused)) char *dest, __attribute__((unused)) char *src)
+{
+
+}
+/**
+ * _atoi - prints char
+ * @s: char to print
+ * Return: always 1
+ */
+
+int _atoi(__attribute__((unused)) char *s)
+{
+
+}
+/**
+ * *_strcat - prints char
+ * @dest: char to print
+ * @src: char to print
+ * Return: always 1
+ */
+
+char *_strcat(__attribute__((unused)) char *dest, __attribute__((unused)) char *src)
+{
+
+}
+/**
+ * *_strncat - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: blabla
+ * Return: always 1
+ */
+
+char *_strncat(__attribute__((unused)) char *dest, __attribute__((unused)) char *src, __attribute__((unused)) int n)
+{
+
+}
+/**
+ * *_strncpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: something
+ * Return: always 1
+ */
+
+char *_strncpy(__attribute__((unused)) char *dest, __attribute__((unused)) char *src, __attribute__((unused)) int n)
+{
+
+}
+/**
+ * _strcmp - prints char
+ * @s1: char to print
+ * @s2: something
+ * Return: always 1
+ */
+
+int _strcmp(__attribute__((unused)) char *s1, __attribute__((unused)) char *s2)
+{
+
+}
+/**
+ * *_memset - prints char
+ * @s: char to print
+ * @b: char to print
+ * @n: something
+ * Return: always 1
+ */
+char *_memset(__attribute__((unused)) char *s, __attribute__((unused)) char b, __attribute__((unused)) unsigned int n)
+{
+
+}
+/**
+ * *_memcpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: something
+ * Return: always 1
+ */
+
+char *_memcpy(__attribute__((unused)) char *dest, __attribute__((unused)) char *src, __attribute__((unused)) unsigned int n)
+{
+
+}
+/**
+ * *_strchr - prints char
+ * @s: char to print
+ * @c: char to print
+ * Return: always 1
+ */
+
+char *_strchr(__attribute__((unused)) char *s, __attribute__((unused)) char c)
+{
+
+}
+/**
+ * _strspn - prints char
+ * @s: char to print
+ * @accept: char to print
+ * Return: always 1
+ */
+
+unsigned int _strspn(__attribute__((unused)) char *s, __attribute__((unused)) char *accept)
+{
+
+}
+/**
+ * *_strpbrk - prints char
+ * @s: char to print
+ * @accept: char to print
+ * Return: always 1
+ */
+
+char *_strpbrk(__attribute__((unused)) char *s, __attribute__((unused)) char *accept)
+{
+
+}
+/**
+ * *_strstr - prints char
+ * @haystack: char to print
+ * @needle: char to print
+ * Return: always 1
+ */
+
+char *_strstr(__attribute__((unused)) char *haystack, __attribute__((unused)) char *needle)
+{
+
+}
